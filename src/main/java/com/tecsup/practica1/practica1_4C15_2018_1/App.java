bec3af12dab784fd008cdc89d8fda958ec533127
Alumno <Alumno@LAB701-11>
2018-03-05 10:49:19 -0500

    Primer cambio

---
+++ src/main/java/com/tecsup/practica1/practica1_4C15_2018_1/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.practica1.practica1_4C15_2018_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
