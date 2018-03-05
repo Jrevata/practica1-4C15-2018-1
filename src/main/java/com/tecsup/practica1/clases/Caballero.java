1ef40cfece5528ad5027051cc54671dc342d90ad
Alumno <Alumno@LAB701-07>
2018-03-05 11:48:12 -0500

    Cambio hecho por Pachacama parte.2

--- src/main/java/com/tecsup/practica1/clases/Caballero.java
+++ src/main/java/com/tecsup/practica1/clases/Caballero.java
@@ -1,5 +1,27 @@
 package com.tecsup.practica1.clases;
+import javax.swing.JOptionPane;
 
 public class Caballero {
+	double precio= 350;
+	String nombrePrenda= "Camisa";
+	String descripción="Color blanco";
+	String talla= "M";
+	String foto="fotocaballero";
+	
+	
+	public void ejerciciocaballero() {
+		
+
+		
+		JOptionPane.showMessageDialog(null, "El nombre del producto es: " + nombrePrenda + 
+				"\n"+ "Descripción: "+ descripción + "\n" + "Talla: "+ talla + "\n" + 
+				"Foto: "+ foto + "\n" + "Precio: " + precio);
+		
+		
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/com/tecsup/practica1/clases/Niño.java
+++ src/main/java/com/tecsup/practica1/clases/Niño.java
@@ -1,5 +1,25 @@
 package com.tecsup.practica1.clases;
 
-public class Niño {
+import javax.swing.JOptionPane;
 
+public class Niño {
+	
+	
+	double precio= 350;
+	String nombrePrenda= "Polo";
+	String foto="fotoniño";
+	
+	
+public void ejerciciocaballero() {
+		
+
+		
+		JOptionPane.showMessageDialog(null, "El nombre del producto es: " + nombrePrenda + 
+				 "\n" + "Foto: "+ foto + "\n" + "Precio: " + precio);
+		
+		
+	}
+	
+	
+	
 }
